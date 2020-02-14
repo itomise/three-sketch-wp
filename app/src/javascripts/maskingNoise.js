7fbb189e2c243d9e590238f2c4275bf0db7acb10
ito ryota <itomise.mizumi@icloud.com>
2020-02-14 10:38:06 +0900

    [update]   とりあえずプッシュ

--- app/src/javascripts/maskingNoise.js
+++ app/src/javascripts/maskingNoise.js
@@ -16,9 +16,14 @@ import { MyNoiseShader } from '../materials/maskingNoise/MyNoiseShader'
 class NoiseSphere {
   constructor(x, y, z) {
     // this.geometry = new THREE.SphereBufferGeometry( 3.5, 60, 60 )
-    this.geometry = new THREE.BoxBufferGeometry(2,10,0,20,20,20)
+    const randomX = Math.random() * 3
+    const randomY = Math.random() * 2
+    const randomZ = Math.random() * 3
+    this.geometry = new THREE.BoxBufferGeometry(randomX,randomY,randomZ,20,20,20)
+    // this.geometry = new THREE.SphereBufferGeometry(randomX, 100, 100)
+    // this.geometry = new THREE.IcosahedronGeometry(10,2);
     this.uniforms = {
-      u_time: { type: 'f', value: 1.0 },
+      u_time: { type: 'f', value: Math.random() * 1000},
       u_resolution: { type: 'v2', value: new THREE.Vector2() },
       u_mouse: { type: 'v2', value: new THREE.Vector2() },
     };
@@ -36,10 +41,16 @@ class NoiseSphere {
     });
     // this.material = new THREE.MeshBasicMaterial()
     this.mesh = new THREE.Mesh(this.geometry, this.material);
+    // this.mesh.position(1, 1, 10)
+    this.mesh.position.x = x
+    this.mesh.position.y = y
+    this.mesh.position.z = z
+    // this.mesh.rotation += 0.1
   }
   draw() {
-    this.uniforms.u_time.value += 0.05;
+    this.uniforms.u_time.value += 0.01;
     // console.log('a')
+    // this.mesh.rotation.x += 0.01
   }
 }
 
@@ -55,11 +66,36 @@ class initial {
   constructor() {
     this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
     this.camera.position.z = 10;
-    var scene1 = new THREE.Scene();
-    var scene2 = new THREE.Scene();
+
+
     // this.noiseSphere = new NoiseSphere(0, 0, 0)
-    this.noiseSphere = new NoiseSphere(0, 0, 0)
-    scene1.add( this.noiseSphere.mesh );
+
+    // ====== scene1 =======
+    var scene1 = new THREE.Scene();
+    this.noiseSphere = [ new NoiseSphere(0, 0, 0), new NoiseSphere(5, 0, 2)]
+
+    this.noiseSphere.forEach( s => {
+      scene1.add(s.mesh)
+    })
+
+
+    // ====== scene2 =======
+    var scene2 = new THREE.Scene();
+    this.sn2Ob = [ new NoiseSphere(0,1,2), new NoiseSphere(2.0,6)]
+    this.sn2Ob.forEach(s => {
+      scene2.add(s.mesh)
+    })
+
+
+    // ====== scene3 =======
+    var scene3 = new THREE.Scene();
+    this.sn3Ob = [ new NoiseSphere(-1, -2, 0), new NoiseSphere(0,-5, 0)]
+    this.sn3Ob.forEach( s => {
+      scene3.add(s.mesh)
+    })
+
+
+
     // var torus = new THREE.Mesh( new THREE.TorusBufferGeometry( 3, 1, 16, 32 ) );
     // scene2.add( torus );
     this.renderer = new THREE.WebGLRenderer();
@@ -73,11 +109,14 @@ class initial {
     var clearMaskPass = new ClearMaskPass();
     var maskPass1 = new MaskPass( scene1, this.camera );
     var maskPass2 = new MaskPass( scene2, this.camera );
+    var maskPass3 = new MaskPass( scene3, this.camera );
     var texture1 = new THREE.TextureLoader().load( redTexture );
     texture1.minFilter = THREE.LinearFilter;
     var texture2 = new THREE.TextureLoader().load( blackTexture );
+    var texture3 = new THREE.TextureLoader().load( yellowTexture );
     var texturePass1 = new TexturePass( texture1 );
     var texturePass2 = new TexturePass( texture2 );
+    var texturePass3 = new TexturePass( texture3 );
     var outputPass = new ShaderPass( CopyShader );
     var parameters = {
       minFilter: THREE.LinearFilter,
@@ -94,6 +133,9 @@ class initial {
     this.composer.addPass( maskPass2 );
     this.composer.addPass( texturePass2 );
     this.composer.addPass( clearMaskPass );
+    this.composer.addPass( maskPass3 );
+    this.composer.addPass( texturePass3 );
+    this.composer.addPass( clearMaskPass );
     this.composer.addPass( outputPass );
     window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
   }
@@ -108,7 +150,15 @@ class initial {
   animate() {
     requestAnimationFrame( this.animate.bind(this) );
     var time = performance.now() * 0.001;
-    this.noiseSphere.draw()
+    this.noiseSphere.forEach(s => {
+      s.draw()
+    })
+    this.sn2Ob.forEach( s => {
+      s.draw()
+    })
+    this.sn3Ob.forEach( s => {
+      s.draw()
+    })
 
     this.renderer.clear();
     this.composer.render( time );
--- app/src/materials/maskingNoise/MyNoiseShader.js
+++ app/src/materials/maskingNoise/MyNoiseShader.js
@@ -12,7 +12,7 @@ var MyNoiseShader = {
 
     "vec3 newPos = position;",
     "newPos.x += sin(position.y + u_time) * 0.3;",
-    "newPos.y += cos(position.x + u_time * 0.9) * 0.3;",
+    "newPos.y += cos(position.x + u_time * 0.4) * 0.3;",
 
 		"	gl_Position = projectionMatrix * modelViewMatrix * vec4( newPos, 1.0 );",
 
